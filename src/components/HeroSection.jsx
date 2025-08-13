624c3fe4ecbec7b79c72a084fcb9dc15dacce751
Jeimie Bobadilla <u2651191@uel.ac.uk>
2025-08-13 21:54:03 +0100

    Update HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -35,13 +35,13 @@ function HeroSection() {
             href="#vision"
             className="bg-blue-600 hover:bg-blue-500 text-white font-semibold py-3 px-6 rounded-full shadow-lg transition duration-300"
           >
-            Discover Our Vision
+            
           </a>
           <button
             onClick={() => setShowModal(true)}
             className="bg-red-600 hover:bg-transparent border-2 border-red-600 text-white font-semibold py-3 px-6 rounded-full shadow-lg transition duration-300"
           >
-            Watch Live
+           
           </button>
         </div>
       </div>
