ab07373134de58e2996315ae0d117d0eb6f6df23
Jeimie Bobadilla <u2651191@uel.ac.uk>
2025-08-22 22:46:18 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
+// src/App.jsx
+import React, { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import Navbar from './components/Navbar';
+import HeroSection from './components/HeroSection';
+import Dashboard from './components/Dashboard';
+
+export default function App() {
+  return (
+    <div className="bg-black text-white min-h-screen">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HeroSection />} />
+        <Route path="/dashboard/:clientid" element={<Dashboard />} />
+      </Routes>
+    </div>
+  );
+}
