dcb96a78003439543a2ad7fe9f0b472879aab69a
Jeimie Bobadilla <u2651191@uel.ac.uk>
2025-08-22 16:29:43 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,19 @@
+// src/App.jsx
 import React, { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 import HeroSection from './components/HeroSection';
 import Dashboard from './components/Dashboard';
-import LiveLoginModal from './components/LiveLoginModal';
 
 export default function App() {
-  const [show_login, set_show_login] = useState(false);
-
   return (
     <div className="bg-black text-white min-h-screen">
-      <Navbar on_open_login={() => set_show_login(true)} />
-
+      <Navbar />
       <Routes>
         <Route path="/" element={<HeroSection />} />
         <Route path="/dashboard/:clientid" element={<Dashboard />} />
       </Routes>
-
-      <LiveLoginModal
-        is_open={show_login}
-        on_close={() => set_show_login(false)}
-      />
     </div>
   );
 }
