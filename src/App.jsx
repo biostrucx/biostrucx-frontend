50acb7266cb988f3b433afd5e60687a815ddd09d
Jeimie Bobadilla <u2651191@uel.ac.uk>
2025-08-25 20:31:40 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
-// src/App.jsx
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import HeroSection from './components/HeroSection';
-import Dashboard from './components/Dashboard';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
+import Launchpad from "./components/Launchpad";
+import GlobalWarming from "./components/GlobalWarming";
 
 export default function App() {
   return (
-    <div className="bg-black text-white min-h-screen">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<HeroSection />} />
-        <Route path="/dashboard/:clientid" element={<Dashboard />} />
-      </Routes>
-    </div>
+    <BrowserRouter>
+      <div className="min-h-screen bg-black text-white">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HeroSection />} />
+          <Route path="/launchpad" element={<Launchpad />} />
+          <Route path="/global-warming" element={<GlobalWarming />} />
+          <Route path="*" element={<div className="p-10">Not Found</div>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
